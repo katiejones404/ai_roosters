486be37b1e1f9610cb7cfbb54119155475b76315
Katie Jones <katie.jones4@outlook.com>
2025-12-03 23:33:43 -0500

    Fixed CORs access issue, removed package and package-lock from .dockerignroe, fixed yml sql path, fixed response path in auth.ts

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,8 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+    #allow_origins=["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://localhost:3000"],
+    allow_origins=["*"], #Safe for now, not safe for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
