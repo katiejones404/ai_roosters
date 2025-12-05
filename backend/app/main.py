cf33518e85c2cfd1074cae9a01d8024c1667a094
SofiaBacha <sofbacha01@gmail.com>
2025-12-05 15:09:39 -0500

    fixed api calls

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api import auth, sentiment, news, portfolio, stocks
+from app.api import auth, sentiment, news, stocks
 
 app = FastAPI(
     title="Stock Portfolio API",
