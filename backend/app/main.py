dbef0fbc220f82ee6463af8b135297919305a525
Kexvin <kdox1023@gmail.com>
2026-01-23 12:19:06 -0500

    uncommited changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, sentiment, news, stocks
 from app.services.prices_ingest import PriceIngestor
-from app.services.sentiment.article_sentiment.article_processing import run_finbert_pipeline_from_env
-from app.services.sentiment.stock_sentiment.stock_processing import run_returns_pipeline
+from app.services.sentiment.article_processing import run_finbert_pipeline_from_env
+from app.services.sentiment.stock_processing import run_returns_pipeline
 from app.services.sentiment.aggregator import run_sentiment_snapshot_pipeline_from_env
 import logging
 import os
